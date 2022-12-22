0f3121b4a3d9d7e2797254b473008e8b9e2b06cc
Rohan das <110011068+ROHANDAS2003@users.noreply.github.com>
2022-12-22 18:58:13 +0530

    Update LMS.c
    Made a file pointer and structs for books and students data.

--- LMS.c
+++ LMS.c
@@ -4,7 +4,24 @@
 #include<time.h>
 #include<string.h>
 
+struct books{
+    int id;
+    char bookName[50];
+    char authorName[50];
+    char date[12];
+}b;
 
+struct student
+{
+    int id;
+    char sName[50];
+    char sClass[50];
+    int sRoll;
+    char bookName[50];
+    char date[12];
+}s;
+
+FILE *fp;
 
 int main(){
     int ch;
