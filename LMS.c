b105d75f0de5e0e2039c17a26bd42244452a547a
Rohan das <110011068+ROHANDAS2003@users.noreply.github.com>
2022-12-23 16:35:44 +0530

    Update LMS.c
    Added the function for showing all issued book's list.

--- LMS.c
+++ LMS.c
@@ -157,6 +157,23 @@ void issueBook(){
     fclose(fp);
 }
 
+void issueList(){
+    system("CLS");
+    printf("<=== Issued Book List ===>\n\n");
+
+    printf("%-10s %-30s %-20s %-10s %-30s %s\n\n", "S.id", "Name", "Class", "Roll", "Book name", "Date");
+
+    fp = fopen("issue.txt", "rb");
+    while (fread(&s, sizeof(s), 1, fp) == 1)
+    {
+        printf("%-10d %-30s %-20s %-10d %-30s %s\n", s.id, s.sName, s.sClass, s.sRoll, s.bookName, s.date);
+    }
+
+    printf("\n");
+    
+    fclose(fp);
+}
+
 int main(){
     int ch;
 
