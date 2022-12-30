7665d20aa9d8de3220594a105815d5220a15af93
Rohan das <110011068+ROHANDAS2003@users.noreply.github.com>
2022-12-30 16:52:21 +0530

    Update Library_Management_System.c
    Added a function to print the content of the software in the center

--- Library_Management_System.c
+++ Library_Management_System.c
@@ -92,6 +92,23 @@ struct person
     int status;
 };
 
+void printMessageCenter(const char *message)
+{
+    int len = 0;
+    int pos = 0;
+
+    // calculate how many space need to print
+    len = (78 - strlen(message)) / 2;
+    printf("\t\t\t");
+    for (pos = 0; pos < len; pos++)
+    {
+        // print space
+        printf(" ");
+    }
+    // print message
+    printf("%s", message);
+}
+
 void headMessage(const char *message)
 {
     system("cls");
